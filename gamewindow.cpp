d22fd3329a1a2abaff6ce3ae158432a4ffc81fa3
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-03-14 01:04:59 -0600

    add running states

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -3,14 +3,17 @@
 
 void GameWindow::setRunning(bool newRunning)
 {
+	_running = newRunning;
 }
 
 bool GameWindow::getRunning()
 {
+	return _running;
 }
 
 GameWindow::GameWindow(bool running)
 {
+	_running = running;
 }
 
 void GameWindow::render()
