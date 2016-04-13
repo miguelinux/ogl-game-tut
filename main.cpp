0e7402bc7cfdaf887dbb32898819ad829f6d0cf0
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-04-12 20:06:16 -0500

    modify title

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main (void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Juego en OpenGL", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
