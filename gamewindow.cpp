a2edb7b49c4048ed6940f77fc9859347430b9e24
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2016-05-06 00:26:22 -0500

    fix typo

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -47,14 +47,14 @@ GLuint GameWindow::loadAndBufferImage(const char *filename)
          * void glTexImage2D(GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLint border, GLenum format,
          *               GLenum type, const GLvoid * data);
 	 */
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,     GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,     GL_CLAMP_TO_EDGE);
 
-	stbi_image_free(data)
+	stbi_image_free(data);
 
 	return textureBufferID;
 }
