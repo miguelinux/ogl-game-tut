431dde7ad0d5aece165586fa823042f4cc046228
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2017-04-29 00:30:00 -0500

    add glew.h on windows

--- myglfw3.h
+++ myglfw3.h
@@ -1,10 +1,20 @@
 #ifndef MYGLFW3_H
 #define MYGLFW3_H
 
-/* To use glGenBuffers and othres */
-#define GL_GLEXT_PROTOTYPES
-#define GLFW_INCLUDE_GLEXT
+/* Copied from GLFW/glfw3.h */
+/* If we are on Windows, we want a single define for it.
+ */
+#if !defined(_WIN32) && (defined(__WIN32__) || defined(WIN32) || defined(__MINGW32__))
+ #define _WIN32
+#endif /* _WIN32 */
 
+#ifdef _WIN32
+  #include <GL/glew.h>
+#else
+  /* To use glGenBuffers and othres */
+  #define GL_GLEXT_PROTOTYPES
+  #define GLFW_INCLUDE_GLEXT
+#endif
 #include <GLFW/glfw3.h>
 
 #endif
